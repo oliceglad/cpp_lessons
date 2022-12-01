409abd3cef56b18aa1fb58eebb39b5ed5dc0f094
Вячеслав Чучманов <vaceslavcucmanov@MacBook-Air-Vaceslav.local>
2022-12-01 21:50:52 +0400

    added new files laba6-1

---
+++ Programs/laba6-1/main.cpp
@@ -0,0 +1,39 @@
+#include "iostream"
+#include "string.h"
+ 
+using namespace std;
+ 
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char Str[80];
+    char* arr[100];
+    char ch[10];
+    int count = 0;
+    char* str;
+    int k = 0;
+
+    
+    cout << "Введите предложение: "; cin.getline(Str, 80);
+
+    str = strtok(Str, " ,.-");
+    while (str != NULL)
+    {
+        arr[k] = str;
+        k++;
+        str = strtok(NULL, " ,.-");
+    }
+
+    for (int i = 0; i < k; i++) {
+        if (i > 1) {
+            if(arr[1][strlen(arr[1])-1] == arr[i][strlen(arr[i])-1]) {
+            count++;
+            cout << arr[i] << endl;
+        }
+        }
+    }
+
+    cout << "Кол-во слов заканчивающиеся на ту же букву что и второе слово: " << count << endl;
+ 
+    return 0;
+}
